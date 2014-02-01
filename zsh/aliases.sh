6e115a4b54b6001cfe326c3a8c694a453485cf9f
Tomas Skogberg <tomas.skogberg@gmail.com>
2014-02-01 18:19:20 +0100

    Escape special characters

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -49,8 +49,8 @@ alias rsp="rake testbot:spec"
 alias g="git"
 alias gs="git status"
 alias gw="git show"
-alias gw^="git show HEAD^"
-alias gw^^="git show HEAD^^"
+alias gw\^="git show HEAD\^"
+alias gw\^\^="git show HEAD\^\^"
 alias gd="git diff-index HEAD -p --color"  # What's changed? Both staged and unstaged.
 alias gdo="git diff --cached"  # What's changed? Only staged (added) changes.
 # for gco ("git commit only") and gca ("git commit all"), see functions.sh.
