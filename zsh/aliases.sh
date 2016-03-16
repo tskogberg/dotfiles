a0214c71fab9ae78aad14aa9cc7c84e7842da7a2
Tomas Skogberg <tomas.skogberg@gmail.com>
2016-03-16 09:17:17 +0100

    Update update gem alias (udgems) for devbox

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -98,7 +98,7 @@ alias rst="touch tmp/restart.txt && echo touched tmp/restart.txt"  # Pow
 # Assumes there is only one screen running.
 alias prodc="ssh pa -t screen -RD"
 
-alias udgems="git pull && dev && bundle update && rake && rake spec && git commit -a -m 'Update gems' && git show && dev stop && echo 'Amend to the commit message if there in any big changes and dont forget to run dev bundle!'"
+alias udgems="git pull && dev && bundle update && (git status | grep 'not staged' &>/dev/null && (test -f Rakefile && rake && rake spec) || git commit -a -m 'Update gems' && git show && echo 'Amend to the commit message if there in any big changes and dont forget to run dev bundle!')|| dev stop"
 # Other
 alias v="vagrant"
 
