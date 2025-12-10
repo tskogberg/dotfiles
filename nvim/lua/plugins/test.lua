dcb49add64176c0012b649ad9b1e7da0b5ab3dab
Tomas Skogberg <tomas.skogberg@gmail.com>
2025-12-10 12:10:09 +0100

    feat: add neotest and other nvim plugin enhancements

---
+++ nvim/lua/plugins/test.lua
@@ -0,0 +1,42 @@
+return {
+  "nvim-neotest/neotest",
+  branch = 'fix/subprocess/load-adapters',
+  lazy = true,
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "olimorris/neotest-rspec",
+    "zidhuss/neotest-minitest",
+    "jfpedroza/neotest-elixir",
+  },
+  keys = {
+    { "<leader>t",  "",                                                                                 desc = "+test" },
+    { "M",          function() require("neotest").run.run(vim.fn.expand("%")) end,                      desc = "Run File" },
+    { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end,                            desc = "Run All Test Files" },
+    { "m",          function() require("neotest").run.run() end,                                        desc = "Run Nearest" },
+    { "<leader>tl", function() require("neotest").run.run_last() end,                                   desc = "Run Last" },
+    { "<leader>ts", function() require("neotest").summary.toggle() end,                                 desc = "Toggle Summary" },
+    { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
+    { "<leader>tO", function() require("neotest").output_panel.toggle() end,                            desc = "Toggle Output Panel" },
+    { "<leader>tS", function() require("neotest").run.stop() end,                                       desc = "Stop" },
+    { "<leader>tw", function() require("neotest").watch.toggle(vim.fn.expand("%")) end,                 desc = "Toggle Watch" },
+  },
+  config = function()
+    require("neotest").setup({
+      log_level = vim.log.levels.DEBUG,
+      adapters = {
+        require("neotest-rspec")({
+          rspec_cmd = function()
+            return vim.tbl_flatten({
+              "bin/rspec",
+            })
+          end,
+        }),
+        require("neotest-minitest"),
+        require("neotest-elixir"),
+      },
+    })
+  end
+}
---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+  {
+    "rcarriga/nvim-notify",
+    keys = {
+      {
+        "<leader>un",
+        function()
+          require("notify").dismiss({ silent = true, pending = true })
+        end,
+        desc = "Dismiss All Notifications",
+      },
+    },
+    opts = {
+      stages = "static",
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 100 })
+      end,
+    },
+    init = function()
+      vim.notify = require("notify")
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  }
+}
+
